3482a423de4e1cf06fefadb3ab6d4efd2ce85b02
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2025-03-17 10:50:53 +0530

    Update Main-gate.tsx

--- src/components/Main-gate.tsx
+++ src/components/Main-gate.tsx
@@ -5,7 +5,7 @@ export default function MainGate() {
           {/* Section Heading */}
           <div className="text-center">
             <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl">
-              Madarsha Main Gate
+              Madarasa Main Gate
             </h2>
           </div>
   
